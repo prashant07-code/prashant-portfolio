07bbe06701ab29ccd2b6f3aeb4d40d46a7a73dcc
Prashant Singh <nfxantaryami@gmail.com>
2026-07-25 21:48:26 +0530

    Fix Gmail SMTP connection using TLS port 587

--- backend/src/controllers/contactController.js
+++ backend/src/controllers/contactController.js
@@ -18,14 +18,17 @@ const contact = async (req, res) => {
     }
 
 
-    // Email transporter
+  
+     // Email transporter
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
 
     // Send email
